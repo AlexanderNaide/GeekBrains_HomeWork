a32af44bee92d7f9b34c6fe33299cb69ef3e9b2e
AlexandrNaide <kb_299@bk.ru>
2022-04-13 17:14:25 +0300

    8 лекция. Чуток дополнил

--- src/main/java/ru/gb/HomeWorks/lesson8/HomeWorkApp.java
+++ src/main/java/ru/gb/HomeWorks/lesson8/HomeWorkApp.java
@@ -2,6 +2,8 @@ package ru.gb.HomeWorks.lesson8;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class HomeWorkApp {
     private static final int WINDOW_WIDTH = 640;
@@ -13,35 +15,61 @@ public class HomeWorkApp {
 
     public static class ExampleWindow extends JFrame{
 
+        private int counter;
+
         public ExampleWindow(){
             setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
             setDefaultCloseOperation(EXIT_ON_CLOSE);
             setLocationRelativeTo(null);
             setTitle("My GUI example");
+            JButton button = new JButton("BUTTON");
+            JButton button2 = new JButton("BUTTON2");
             JPanel panel = new JPanel();
             panel.setLayout(new FlowLayout());
-            JButton button = new JButton("BUTTON");
             panel.add(button, BorderLayout.NORTH);
-            JButton button2 = new JButton("BUTTON2");
             panel.add(button2, BorderLayout.SOUTH);
             add(panel, BorderLayout.SOUTH);
 
             JMenuBar bar = new JMenuBar();
             JMenu fileMenu = new JMenu("File");
-            JMenuItem createItem = new JMenuItem("Create");
-            JMenuItem exitItem = new JMenuItem("Exit");
             JMenu editMenu = new JMenu("Edit");
             JMenu helpMenu = new JMenu("Help");
+            JMenuItem createItem = new JMenuItem("Create");
+            JMenuItem exitItem = new JMenuItem("Exit");
+
 
             bar.add(fileMenu);
             bar.add(editMenu);
             bar.add(helpMenu);
 
+            fileMenu.add(createItem);
+            fileMenu.add(exitItem);
+
             add(bar, BorderLayout.NORTH);
 
+            exitItem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.exit(1);
+                }
+            });
+
             JLabel label = new JLabel("Press the button");
-            label.setFont(new Font("Times new Roman", Font.BOLD, 12));
+            label.setFont(new Font("Times new Roman", Font.BOLD, 50));
             add(label);
+
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    counter++;
+                    label.setText("COUNTER: " + counter) ;
+                }
+            });
+
+            button2.addActionListener((e) -> {
+                counter--;
+                label.setText("COUNTER: " + counter) ;
+            });
 
 
 
