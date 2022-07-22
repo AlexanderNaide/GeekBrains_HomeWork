052ffd0fa9e9905a2f9f496d24bbf44a296715e0
AlexandrNaide <kb_299@bk.ru>
2022-07-22 16:53:01 +0300

    Bootcamp - Algorithm_5 закончен

--- src/main/java/ru/gb/Bootcamp/Algorithms/Armstrong/Logger.java
+++ src/main/java/ru/gb/Bootcamp/Algorithms/Armstrong/Logger.java
@@ -1,17 +1,23 @@
 package ru.gb.Bootcamp.Algorithms.Armstrong;
 
 import java.util.Date;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Logger {
 
     private final Date startDate = new Date();
     public static boolean show = false;
+    private final Map<Integer, Long> results = new TreeMap<>();
 
     public void log(int value){
+        results.putIfAbsent(value, new Date().getTime() - startDate.getTime());
+    }
+    public void print(){
         if (show){
-            long diffTimes = new Date().getTime() - startDate.getTime();
-//        System.out.println((diffTimes / 1000) + " found armstrong number: " + value);
-            System.out.println((diffTimes) + " found armstrong number: " + value);
+            for (Map.Entry<Integer, Long> integerLongEntry : results.entrySet()) {
+                System.out.println((integerLongEntry.getValue()) + " found armstrong number: " + integerLongEntry.getKey());
+            }
         }
     }
 }
