7bda854d73c578c4a7da2d2d951b0ae861e8c54a
AlexandrNaide <kb_299@bk.ru>
2022-09-01 22:04:45 +0300

    C3_L6 start

---
+++ src/main/java/ru/gb/HomeWorks_core3/lesson6/HomeWorkApp.java
@@ -0,0 +1,14 @@
+package ru.gb.HomeWorks_core3.lesson6;
+
+import com.sun.tools.javac.Main;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.util.Loader;
+
+public class HomeWorkApp {
+//    private static final Logger logger = Logger.getLogger("");
+    private static Logger logger = LogManager.getLogger(Main.class);
+    public static void main(String[] args) {
+
+    }
+}
