1537661390c14b43a984cb4753d6f4cb9f3674d5
AlexandrNaide <kb_299@bk.ru>
2022-09-05 16:23:52 +0300

    C3_L6 разобрал логирование

--- src/main/java/ru/gb/HomeWorks_core3/lesson6/HomeWorkApp.java
+++ src/main/java/ru/gb/HomeWorks_core3/lesson6/HomeWorkApp.java
@@ -1,14 +1,41 @@
 package ru.gb.HomeWorks_core3.lesson6;
 
-import com.sun.tools.javac.Main;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.util.Loader;
+//import org.apache.logging.log4j.core.Logger;
+
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+//import java.util.logging.Logger;
 
 public class HomeWorkApp {
 //    private static final Logger logger = Logger.getLogger("");
-    private static Logger logger = LogManager.getLogger(Main.class);
+    private static final Logger logger = LogManager.getLogger(HomeWorkApp.class);
     public static void main(String[] args) {
+/*        logger.getHandlers()[0].setFormatter(new Formatter() {
+            @Override
+            public String format(LogRecord record) {
+                return record.getLevel() + "\t" + record.getMessage() + "\t" + record.getMillis() + "\n";
+            }
+        });
+
+        logger.log(Level.INFO, "Connection");
+        logger.setLevel(Level.WARNING);*/
+
+
+        logger.info("Info");
+
+        if(logger.isDebugEnabled()) {
+            logger.debug("Debug");
+        }
+
+        logger.info("Info message {}", "(Insert message)");
+        logger.warn("Warning");
+        logger.error("Error");
+
+
 
     }
 }
