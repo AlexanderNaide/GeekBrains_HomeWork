c91d465e1bb05a4288472fabe0514074b75ce626
AlexanderNaide <kb_299@bk.ru>
2022-12-07 22:50:30 +0300

    SpringContext ClassWork дополнения

--- JavaEE_and_SPRING/src/main/java/ru/gb/SpringContext/ClassWork/MainClass.java
+++ JavaEE_and_SPRING/src/main/java/ru/gb/SpringContext/ClassWork/MainClass.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class MainClass {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("ru.gb.SpringContext.ClassWork");
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("ru.gb.SpringContext.ClassWork");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
 /*            Box box1 = context.getBean(Box.class);
             Box box2 = context.getBean(Box.class);
@@ -17,6 +18,9 @@ public class MainClass {
             System.out.println(box2.getColor());*/
 
         OrderService orderService = context.getBean(OrderService.class);
+        orderService.createOrderFromProduct(1L);
+        orderService.createOrderFromProduct(1L);
+        orderService.createOrderFromProduct(1L);
         orderService.createOrderFromProduct(1L);
 
         context.close();
