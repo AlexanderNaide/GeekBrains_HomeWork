037c40d6144dbacbb4eafffc927dead3f1f7d12f
AlexanderNaide <kb_299@bk.ru>
2022-12-08 21:35:02 +0300

    Комментарий

--- JavaEE/src/main/java/ru/gb/JavaEE/EncodingFilter.java
+++ JavaEE/src/main/java/ru/gb/JavaEE/EncodingFilter.java
@@ -4,7 +4,7 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = "/*")
+@WebFilter(urlPatterns = "/*")  // тут также можно фильтроваться по запросу. В данном случае берутся все запросы, которые заходят за /
 public class EncodingFilter implements Filter {
 
     private FilterConfig config;
@@ -18,7 +18,9 @@ public class EncodingFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         servletResponse.setContentType("text/html");
         servletResponse.setCharacterEncoding("UTF-8");
-        filterChain.doFilter(servletRequest, servletResponse);
+        filterChain.doFilter(servletRequest, servletResponse); // - это значит, что фильтр пробросил запрос дальше без изменений.
+        // На самом деле фильтром тут можно модифицировать запросы и ответы, проверять параметры запроса.
+        // Одно из назначений фильтра - проверять например авторизацию и перекидывать на определенные сервлеты в зависимости от запроса
     }
 
     @Override
